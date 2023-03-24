4a95a7fd6849dea826a5e038b1dff15b3229498c
arifrahmadi23 <muhammad231arif@gmail.com>
2023-03-24 10:32:58 +0700

    membuat tampilan Masukan Element Array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -17,3 +17,8 @@ void input() {				// Procedure Input
 		{
 			cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; // menampilkan Pesan jika data lebih dari 20
 		}
+	}
+	cout << endl;									// Membuat jarak per baris program
+	cout << "======================" << endl;		// Membuat tampilan susuna data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
