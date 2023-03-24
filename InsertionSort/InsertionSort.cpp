9d47bfa8477f2ef3b089cdcb638aac8cf26131a0
arifrahmadi23 <muhammad231arif@gmail.com>
2023-03-24 10:39:04 +0700

    memberikan perintah perulangan  untuk menyimpan data pada array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -22,3 +22,12 @@ void input() {				// Procedure Input
 	cout << "======================" << endl;		// Membuat tampilan susuna data element array
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
+
+
+	for (int i + 0; i < n; i++)						// Mengunnakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";		// Memasukan atau menginputkan nilai data n
+		cin >> arr[i];								// Menyimpan nilai data n kedalam array arr
+	}
+}
+
